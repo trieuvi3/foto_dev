853fd626b568530e6261dcd450e072baef0dd72d
TrieuVi <trieuvi5@gmail.com>
2022-12-21 20:12:02 +0700

    change login logout

--- src/app/controller/UserController.js
+++ src/app/controller/UserController.js
@@ -70,7 +70,7 @@ class UserController {
         try {
             const user = await User.create({ email, password})
             const token = createToken(user._id);
-            res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000});
+            // res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000});
             res.status(201).json({user: user._id});
         }
         catch(err) {
